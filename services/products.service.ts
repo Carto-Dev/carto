9d46583e61f39a48e60c837837bf9030a6bc3627
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2022-02-05 12:55:22 +0530

    Service Implementation for updating a product. Closes #53

--- services/products.service.ts
+++ services/products.service.ts
@@ -1,3 +1,4 @@
+import {UpdateProductDto} from './../dtos/products/update-product.dto';
 import NetInfo from '@react-native-community/netinfo';
 import {ProductModel} from './../models/product.model';
 import {CreateProductDto} from './../dtos/products/create-product.dto';
@@ -36,6 +37,54 @@ export const createProduct = async (
 
     // Send request to the server.
     const response = await server.post('v1/product', body, {
+      headers,
+    });
+
+    // Get response data from request.
+    const responseJson = response.data;
+
+    // Convert response data to Product.
+    const product = new ProductModel();
+    product.fromJson(responseJson);
+
+    // Return Product.
+    return product;
+  } catch (error: unknown) {
+    console.log(error);
+    throw new Error(AuthError.INTERNAL_SERVER_ERROR);
+  }
+};
+
+/**
+ * Method to update products on server.
+ * @param updateProductDto DTO For creating a new product
+ * @returns ProductModel details for new product
+ */
+export const updateProduct = async (
+  updateProductDto: UpdateProductDto,
+): Promise<ProductModel> => {
+  const connection = await NetInfo.fetch();
+
+  if (!connection.isConnected) {
+    console.log('Not connected to the internet');
+    throw new Error(Connectivity.OFFLINE);
+  }
+
+  try {
+    // Prepare request body.
+    const body = updateProductDto.toJson();
+
+    // Get Firebase Token
+    const firebaseToken = await authService.currentUser().getIdToken();
+
+    // Prepare request headers.
+    const headers = {
+      'content-type': 'application/json',
+      authorization: `Bearer ${firebaseToken}`,
+    };
+
+    // Send request to the server.
+    const response = await server.put('v1/product', body, {
       headers,
     });
 
