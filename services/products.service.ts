4ecd49d183d5e9d3a9734840cf7e8bd41895411e
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2022-02-05 13:44:22 +0530

    Service Implementation for fetching products by user. Closes #58

--- services/products.service.ts
+++ services/products.service.ts
@@ -56,8 +56,9 @@ export const fetchCategories = async (): Promise<CategoryModel[]> => {
 };
 
 /**
- * Method to fetch categories.
- * @returns CategoryModel[] categories array
+ * Method to fetch products by category.
+ * @param category Category to fetch posts for.
+ * @returns ProductModel[] products array
  */
 export const fetchProductsByCategory = async (
   category: string,
@@ -83,6 +84,58 @@ export const fetchProductsByCategory = async (
     const response = await server.get('v1/product/category', {
       params: {
         category,
+      },
+      headers,
+    });
+
+    // Get response data from request.
+    const responseJson = response.data;
+
+    // Convert response data to products array.
+    const products: ProductModel[] = responseJson.map((productJson) => {
+      const product = new ProductModel();
+      product.fromJson(productJson);
+
+      return product;
+    });
+
+    // Return products.
+    return products;
+  } catch (error: unknown) {
+    console.log(error);
+    throw new Error(AuthError.INTERNAL_SERVER_ERROR);
+  }
+};
+
+/**
+ * Method to fetch products by user.
+ * @param userId User ID to fetch posts from.
+ * @returns ProductModel[] products array
+ */
+export const fetchProductsByUser = async (
+  userId: number,
+): Promise<ProductModel[]> => {
+  const connection = await NetInfo.fetch();
+
+  if (!connection.isConnected) {
+    console.log('Not connected to the internet');
+    throw new Error(Connectivity.OFFLINE);
+  }
+
+  try {
+    // Get Firebase Token
+    const firebaseToken = await authService.currentUser().getIdToken();
+
+    // Prepare request headers.
+    const headers = {
+      'content-type': 'application/json',
+      authorization: `Bearer ${firebaseToken}`,
+    };
+
+    // Send request to the server.
+    const response = await server.get('v1/product/user', {
+      params: {
+        userId,
       },
       headers,
     });
