8237e0e6077e0872b2a1517464891336ea43ac57
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2022-02-05 12:49:00 +0530

    DTO Implementation for creation of a product. Closes #50

---
+++ dtos/products/create-product.dto.ts
@@ -0,0 +1,31 @@
+export class CreateProductDto {
+  public title: string = '';
+  public description: string = '';
+  public cost: number = 0;
+  public imgLinks: string[] = [];
+  public categories: string[] = [];
+
+  fromJson(json: any): void {
+    this.title = json.title ?? '';
+    this.description = json.description ?? '';
+    this.cost = json.cost ?? 0;
+    this.imgLinks = json.imgLinks ?? [];
+    this.categories = json.categories ?? [];
+  }
+
+  toJson(): {
+    title: string;
+    description: string;
+    cost: number;
+    imgLinks: string[];
+    categories: string[];
+  } {
+    return {
+      title: this.title,
+      description: this.description,
+      cost: this.cost,
+      imgLinks: this.imgLinks,
+      categories: this.categories,
+    };
+  }
+}
