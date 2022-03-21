81d6e942b5e8bb47ddf29906f9643c5e1c89e0d3
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2022-03-21 09:04:36 +0530

    Update MyReview.tsx

--- pages/products/MyReview.tsx
+++ pages/products/MyReview.tsx
@@ -2,8 +2,10 @@ import React, {useEffect, useState} from 'react';
 import {FlatList, View, StyleSheet, RefreshControl} from 'react-native';
 import {Title} from 'react-native-paper';
 import EmptyDataAnimation from '../../components/Lottie/EmptyDataAnimation';
+import ErrorAnimation from '../../components/Lottie/ErrorAnimation';
 import LoadingAnimation from '../../components/Lottie/LoadingAnimation';
 import UserReviewCardComponent from '../../components/Review/UserReviewCard';
+import {Connectivity} from '../../enum/connectivity-error.enum';
 import {UserReview} from '../../models/user-review.model';
 import * as reviewService from './../../services/reviews.service';
 
@@ -15,11 +17,23 @@ const MyReviewsPage: React.FC = () => {
   const [userReviews, setUserReviews] = useState<UserReview[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
 
+  const [errorMessage, setErrorMessage] = useState<string>('');
+  const [error, setError] = useState<boolean>(false);
+
   const loadUserReviews = (mounted: boolean) =>
     reviewService
       .fetchReviewsByUser()
       .then((reviews) => (mounted ? setUserReviews(reviews) : null))
-      .catch((error) => console.log(error))
+      .catch((error) => {
+        if (error.message === Connectivity.OFFLINE.toString()) {
+          setErrorMessage('You are offline');
+        } else {
+          console.log(error);
+          setErrorMessage('Something went wrong, please try again later');
+        }
+
+        setError(true);
+      })
       .finally(() => (mounted ? setLoading(false) : null));
 
   // Listen to firebase state changes for reviews.
@@ -32,6 +46,10 @@ const MyReviewsPage: React.FC = () => {
       mounted = false;
     };
   }, []);
+
+  if (error) {
+    return <ErrorAnimation message={errorMessage} />;
+  }
 
   return loading ? (
     <LoadingAnimation message="Loading Reviews" />
