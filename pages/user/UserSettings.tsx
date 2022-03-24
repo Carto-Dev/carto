490b45610c6ad575f62a10ffdd110f191d1c2f3c
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2022-03-24 12:39:46 +0530

    UI Implementation for updating user profile. Closes #148

--- pages/user/UserSettings.tsx
+++ pages/user/UserSettings.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import {StyleSheet, View} from 'react-native';
+import UserProfileSettings from './UserProfleSettings';
 
 const UserSettings: React.FC = () => {
-  return <View></View>;
+  return (
+    // <View style={styles.rootView}>
+    <UserProfileSettings />
+    // </View>
+  );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  rootView: {
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+});
 
 export default UserSettings;
