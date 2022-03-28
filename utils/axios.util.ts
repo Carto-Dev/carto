c0211825da4564c72501fae461d5969c97aac90d
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2022-03-28 15:56:47 +0530

    API Url check for dev and prod

--- utils/axios.util.ts
+++ utils/axios.util.ts
@@ -1,6 +1,14 @@
 import axios from 'axios';
 
+if (__DEV__) {
+  console.log('Development');
+} else {
+  console.log('Production');
+}
+
 export const server = axios.create({
-  baseURL: 'http://10.0.2.2:5000/api/',
+  baseURL: __DEV__
+    ? 'http://10.0.2.2:5000/api/'
+    : 'https://carto-production.herokuapp.com/api',
   timeout: 5000,
 });
